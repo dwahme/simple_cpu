0945c1fc86bf77211e279acafa7aecbd51c7c683
Dawson Whitehead <dwahme1@gmail.com>
2019-07-07 14:38:32 -0500

    Added simple class outlines for CPU and memory

---
+++ simple_cpu/cpu.py
@@ -0,0 +1,21 @@
+
+class CPU:
+    
+    def __init__(self):
+        pass
+
+    def instr_fetch():
+        pass
+
+    def intr_decode():
+        pass
+
+    def execute():
+        pass
+
+    def mem():
+        pass
+
+    def writeback():
+        pass
+
