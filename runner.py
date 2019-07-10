026917233a00c29b56cf7a7b344f2fb10bcd7f95
Dawson Whitehead <33361651+dwahme@users.noreply.github.com>
2019-07-10 09:16:31 -0500

    Added better commandline running

--- runner.py
+++ runner.py
@@ -1,13 +1,34 @@
+import argparse
 import sys
 from simple_cpu import CPU, Assembler, Memory
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a SAL program")
+
+    parser.add_argument('-f', action='store', dest='file', type=str, 
+                        help='The file to process')
+    parser.add_argument('-a', action='store', dest='assembled', type=str, default="",
+                        help='Where to store the assembled code')
+    parser.add_argument('-q', action='store_true', default=False,
+                        dest='quiet',
+                        help='Only print final CPU state')
+    parser.add_argument('-s', action='store_true', default=0,
+                        dest='lo',
+                        help='Low end of memory region to dump')
+    parser.add_argument('-e', action='store_true', default=10,
+                        dest='hi',
+                        help='High end of memory region to dump')
+
+    results = parser.parse_args()
+
     a = Assembler()
-    a.assemble("samples/simple.sal", "out.txt")
+    if results.assembled == "":
+        assembled = a.assemble(results.file)
+    else:
+        assembled = a.assemble(results.file, results.assembled)
 
     m = Memory()
-    m.load_file("out.txt")
-    m.dump(stop=10)
+    m.load(assembled)
 
     c = CPU(m)
-    c.run(True)
+    c.run(not results.quiet, results.lo, results.hi)
