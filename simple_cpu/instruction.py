9fb4ad3d3a1d2b48dcecdda452fabfd014595f98
Dawson Whitehead <dwahme1@gmail.com>
2019-07-10 00:50:43 -0500

    Added string representation for Instructions

--- simple_cpu/instruction.py
+++ simple_cpu/instruction.py
@@ -9,3 +9,7 @@ class Instruction:
 
     def __init__(self, encoded):
         self.instr_encoded = encoded
+
+    def __repr__(self):
+        strs = [str(x) for x in self.args]
+        return "{} {}".format(self.op, " ".join(strs))
